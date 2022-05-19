8f6fd0c6f114be6bed8dfe128ed09a6dd2bc95eb
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2022-05-19 14:00:55 +0200

    Front-end events

--- fiame-desk/resources/views/events.blade.php
+++ fiame-desk/resources/views/events.blade.php
@@ -1,3 +1,23 @@
 <x-app-layout>
-
+    <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
+        <div class="max-w-md py-5 px-5 bg-white shadow-lg rounded-lg mx-20 my-20">
+            <span class="">9 juin 2022</span><br>
+            <div class="my-4">Place du marché Yvedon</div>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="max-w-md py-24 px-5 bg-white shadow-lg rounded-lg mx-20 my-20">
+            2
+        </div>
+        <div class="max-w-md py-24 px-5 bg-white shadow-lg rounded-lg mx-20 my-20">
+            3
+        </div>
+        <div class="max-w-md py-24 px-5 bg-white shadow-lg rounded-lg mx-20 my-20">
+            4
+        </div>
+        <div class="max-w-md py-24 px-5 bg-white shadow-lg rounded-lg mx-20 my-20">
+            5
+        </div>
+    </div>
 </x-app-layout>
