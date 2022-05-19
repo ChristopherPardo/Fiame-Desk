48975eaa81135aec22319face9bbe730039cdcab
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2022-05-19 15:48:35 +0200

    User not admin

--- fiame-desk/database/seeders/UserSeeder.php
+++ fiame-desk/database/seeders/UserSeeder.php
@@ -17,13 +17,27 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'firstname' => 'Christopher',
-            'lastname' => 'Pardo',
-            'phone' => '0798765432',
-            'admin' => 1,
-            'token' => Str::random(30),
-            'password' => Hash::make('Pa$$w0rd'), // password
-        ]);
+        $users = [
+            [
+                'firstname' => 'Christopher',
+                'lastname' => 'Pardo',
+                'phone' => '0798765432',
+                'admin' => 1,
+                'token' => Str::random(30),
+                'password' => Hash::make('Pa$$w0rd'), // password
+            ],
+            [
+                'firstname' => 'David',
+                'lastname' => 'Roulet',
+                'phone' => '0723456789',
+                'admin' => 0,
+                'token' => Str::random(30),
+                'password' => Hash::make('Pa$$w0rd'), // password
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
